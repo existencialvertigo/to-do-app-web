495e8fe3cfdcbbd14aa26f8ff8c58f8748d7c8db
existencialvertigo <gabrielportoanunessouza@gmail.com>
2023-03-21 08:37:48 -0300

    Revert "Committed"
    This reverts commit 1fe9d2236586b976852fbca98948fb2e2144a157.

--- Exercises/ExercisesDay14.py
+++
@@ -1,7 +0,0 @@
-# Exercise 1
-# from moduleDay14 import water_state
-import moduleDay14
-temperature = int(input("Enter water temperature: "))
-moduleDay14.water_state(temperature)
-
-# Exercise 2 is in moduleDay14
