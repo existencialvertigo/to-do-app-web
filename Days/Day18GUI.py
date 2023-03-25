4f13f3303d10b5f52f8323a6c0524762e8a4914b
existencialvertigo <gabrielportoanunessouza@gmail.com>
2023-03-25 17:15:25 -0300

    Implement the complete and exit buttons

---
+++ Days/Day18GUI.py
@@ -0,0 +1,52 @@
+import Day14functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=Day14functions.get_todos(), key="todos",
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
+
+window = sg.Window("My To-Do App",
+                   layout=layout,
+                   font=("Helvetica", 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = Day14functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo.capitalize())
+            Day14functions.write_todos(todos)
+            window["todos"].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = Day14functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            Day14functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = Day14functions.get_todos()
+            todos.remove(todo_to_complete)
+            Day14functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+window.close()
